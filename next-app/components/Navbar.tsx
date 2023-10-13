ff130d896f79bc83e21382b7e28a58a76af7a849
mohamedatef73 <matef4202@gmail.com>
2023-10-13 15:52:39 +0300

    preparing navbar

---
+++ next-app/components/Navbar.tsx
@@ -0,0 +1,30 @@
+import Image from "next/image"
+import Link from "next/link"
+import { NAV_LINKS } from "@/constants"
+import Button from "./Button"
+
+const Navbar = () => {
+  return (
+    <nav className="flexBetween  max-container padding-container relative z-30 py-5">
+      <Link href="/">
+        <Image src="/hilink-logo.svg" alt="logo" width={74} height={29} />
+      </Link>
+      <ul className="hidden h-full gap-12 lg:flex ">
+        {NAV_LINKS.map((link) => (
+          <Link
+            className="regular-16 text-gray-50 flexCenter cursor-pointer pb1.5 transition-all hover:font-bold"
+            href={link.href}
+            key={link.key}
+          >
+            {link.label}
+          </Link>
+        ))}
+      </ul>
+      <div className="lg:flexCenter hidden">
+        <Button type="button" title="login" icon="/user.svg" variant="btn_dark_green" />
+      </div>
+    </nav>
+  )
+}
+
+export default Navbar
